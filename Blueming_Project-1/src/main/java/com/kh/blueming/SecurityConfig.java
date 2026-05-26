7cbe2e35fe8c4975a12959513939ad166fb31bb6
papo1022 <85226430+papo1022@users.noreply.github.com>
2026-05-26 14:05:26 +0900

    공통 코드 추가
    다음과 같은 코드가 포함되어 있음
    - PageInfo.java : 페이지 수를 계산하는 7개의 변수가 담긴 vo
    - SecurityConfig : security를 사용하면서 로그인 화면을 막는 코드
    - Pagaination : PageInfo의 변수를 가지고 계산해서 7개의 변수를 리턴하는 코드
    - XssDefencePolicty.java : Xss 공격을 막는 코드
    
    한번 fetch 해보고 안되는거 있으면 말해주기 바랍람

---
+++ Blueming_Project-1/src/main/java/com/kh/blueming/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.kh.blueming;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	// Spring Security 디펜던시에서 로그인 화면이 뜨는걸 막는 코드입니다.
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+		return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+				.csrf(csrf -> csrf.disable()).build();
+	}
+}
