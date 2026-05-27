5b99bd1966db0bf5cd797ec15aec5f375ad7f472
jaeugjeong15-wq <jaeugjeong15@gmail.com>
2026-05-27 09:44:46 +0900

    0527

---
+++ Blueming_Project-1/src/main/java/com/kh/blueming/member/model/service/MemberService.java
@@ -0,0 +1,25 @@
+package com.kh.blueming.member.model.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.blueming.member.model.vo.Member;
+
+@Service
+public class MemberService {
+	
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private MemberDao memberDao;
+	
+	
+	public Member loginMember(Member m) {
+		
+		return memberDao.loginMember(sqlSession, m);
+	}
+
+}
