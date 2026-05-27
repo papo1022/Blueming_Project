e21f30b43d932373509b5fbd1a2e3285ee6a9175
jaeugjeong15-wq <jaeugjeong15@gmail.com>
2026-05-27 16:36:51 +0900

    0527 16:50

--- Blueming_Project-1/src/main/java/com/kh/blueming/notice/model/controller/NoticeController.java
+++ Blueming_Project-1/src/main/java/com/kh/blueming/notice/model/controller/NoticeController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.blueming.notice.model.service.NoticeService;
 import com.kh.blueming.notice.model.vo.Notice;
 
 @Controller
