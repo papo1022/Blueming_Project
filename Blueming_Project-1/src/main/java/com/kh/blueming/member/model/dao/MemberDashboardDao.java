6cf1be9bb097afb249757dba51f94bf75c7bce95
Jia-Lim11 <jihyelim331@gmail.com>
2026-06-01 14:04:28 +0900

    pom.xml 수정
    mybatis 사용을 위한 세팅

--- Blueming_Project-1/src/main/java/com/kh/blueming/member/model/dao/MemberDashboardDao.java
+++ Blueming_Project-1/src/main/java/com/kh/blueming/member/model/dao/MemberDashboardDao.java
@@ -1,5 +1,19 @@
 package com.kh.blueming.member.model.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.blueming.member.model.vo.Member;
+
+@Repository
 public class MemberDashboardDao {
 
+	@Autowired
+	private SqlSession sqlSession;
+	
+	// 사원 정보 조회
+	public Member getMemberInfo(int memberId) {{
+		
+		return sqlSession.selectOne("memberDashboard.getMemberInfo", memberId);
+	}
 }
