e1385ed0c6d7c097f4411034a8f5a094f05f33a9
Jia-Lim11 <jihyelim331@gmail.com>
2026-06-04 11:09:31 +0900

    사원 대시보드- Service단 구현
    Dao 클래스에 맞춰 Service단 코드 구현

--- Blueming_Project-1/src/main/java/com/kh/blueming/member/model/dao/MemberDashboardDao.java
+++ Blueming_Project-1/src/main/java/com/kh/blueming/member/model/dao/MemberDashboardDao.java
@@ -1,29 +1,35 @@
 package com.kh.blueming.member.model.dao;
 
-import org.apache.ibatis.session.SqlSession;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.kh.blueming.member.model.vo.Member;
+import com.kh.blueming.assignment.model.vo.AssignmentCard;
+import com.kh.blueming.course.model.vo.CourseCard;
+import com.kh.blueming.member.model.vo.MemberProfile;
 
 @Repository
 public class MemberDashboardDao {
 
-	private SqlSession sqlSession;
+	@Autowired
+	private SqlSessionTemplate sqlSession;
 	
 	// 1. 회원 프로필 조회
-	public Member selectMemberProfile(int memberId) {
+	public MemberProfile selectMemberProfile(int memberId) {
 		
 		return sqlSession.selectOne("memberDashboardMapper.selectMemberProfile", memberId);
 	}
 	
 	// 2. 내 강의 목록 조회(진도율, D-day, 상태)
-	public List<Map<String, object>> selectMyCourseList(int memberId) {
+	public List<CourseCard> selectMyCourseList(int memberId) {
 		
 		return sqlSession.selectList("memberDashboardMapper.selectMyCourseList", memberId);
 	}
 	
 	// 3. 내 과제 목록 조회(제출 여부)
-	public List<> selectMyAssignmentList(int memberId) {
+	public List<AssignmentCard> selectMyAssignmentList(int memberId) {
 		
 		return sqlSession.selectList("memberDashboardMapper.selectMyAssignmentList", memberId);
 	}
--- Blueming_Project-1/src/main/java/com/kh/blueming/member/model/service/MemberDashboardService.java
+++ Blueming_Project-1/src/main/java/com/kh/blueming/member/model/service/MemberDashboardService.java
@@ -1,5 +1,35 @@
 package com.kh.blueming.member.model.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.kh.blueming.assignment.model.vo.AssignmentCard;
+import com.kh.blueming.course.model.vo.CourseCard;
+import com.kh.blueming.member.model.dao.MemberDashboardDao;
+import com.kh.blueming.member.model.vo.MemberProfile;
+
+@Service
 public class MemberDashboardService {
 
+	private MemberDashboardDao memberDashboardDao;
+	
+	// 1. 회원 프로필 조회
+	public MemberProfile selectMemberProfile(int memberId) {
+		
+		return memberDashboardDao.selectMemberProfile(memberId);
+	}
+	
+	// 2. 내 강의 목록 조회
+	public List<CourseCard> selectMyCourseList(int memberId) {
+		
+		return memberDashboardDao.selectMyCourseList(memberId);
+	}
+	
+	// 3. 내 과제 목록 조회
+	public List<AssignmentCard> selectMyAssignmentList(int memberId) {
+		
+		return memberDashboardDao.selectMyAssignmentList(memberId);
+	}
+	
 }
